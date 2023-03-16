7d22c9d881b6fb4504b3f922d2f7f37a9ae0a0ff
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2023-03-16 20:38:49 +0000

    Add comment

--- demo/level.gd
+++ demo/level.gd
@@ -8,4 +8,6 @@ func _on_timer_timeout() -> void:
 	var random_position = Vector3(randf_range(0, 10), 0, randf_range(0, 10))
 	
 	SFX.play_at_location("footsteps/boot_[%n]", random_position)
+	
+	# Restart the timer to emulate walking.
 	$Timer.start()
